758ced09f5ac2d0f3193aaf58d0c78348c3da96d
Marissa Aragon <marag27@wgu.edu>
2025-05-31 11:41:02 -0600

    Sort Practice

---
+++ sorts/BubbleSort.py
@@ -0,0 +1,16 @@
+def bubble_sort(my_list):
+    for i in range(len(my_list)-1, 0, -1):
+        for j in range(i):
+            if my_list[j] > my_list[j+1]:
+                temp = my_list[j]
+                my_list[j] = my_list[j+1]
+                my_list[j+1] = temp
+    return my_list
+
+
+
+list = [5,1,4,6,7,2,8,3]
+
+bubble_sort(list)
+
+print(list)
---
+++ sorts/SelectionSort.py
@@ -0,0 +1,17 @@
+def selection_sort(my_list):
+    for i in range(len(my_list)-1):
+        min_index = i
+        for j in range(i+1, len(my_list)):
+            if my_list[j] < my_list[min_index]:
+                min_index = j
+        if i != min_index:
+            temp = my_list[i]
+            my_list[i] = my_list[min_index]
+            my_list[min_index] = temp
+    return my_list
+
+my_list = [5,1,4,6,7,2,8,3]
+
+selection_sort(my_list)
+
+print(my_list)
